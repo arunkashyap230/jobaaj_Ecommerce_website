bf48a08f46069cccc7fd6c8c6265b2f8b08d2695
Arun kashyap <arunkashyap230@gmail.com>
2026-05-17 15:10:06 +0530

    cloudinary fix

--- server/routes/uploadRoutes.js
+++ server/routes/uploadRoutes.js
@@ -1,29 +1,38 @@
 const express = require("express");
-
 const router = express.Router();
 
 const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
 
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
-
-const cloudinary = require("../config/cloudinary");
-
-// Cloudinary Storage
-const storage = new CloudinaryStorage({
-  cloudinary,
-
-  params: {
-    folder: "ecommerce-products",
-
-    allowed_formats: ["jpg", "png", "jpeg", "webp"],
-  },
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+const storage = multer.memoryStorage();
 
 const upload = multer({ storage });
 
-// Upload Route
-router.post("/", upload.single("image"), (req, res) => {
-  res.send(req.file.path);
+router.post("/", upload.single("image"), async (req, res) => {
+  try {
+    const result = await cloudinary.uploader.upload_stream(
+      {
+        folder: "ecommerce-products",
+      },
+      (error, result) => {
+        if (error) {
+          return res.status(500).json({ message: error.message });
+        }
+
+        res.send(result.secure_url);
+      },
+    );
+
+    result.end(req.file.buffer);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 module.exports = router;
